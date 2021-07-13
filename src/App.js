72c11d1483b5e7ec92e6fc7a7b75374b1c484447
unknown <viniciuslisboa1001@gmail.com>
2021-07-13 16:56:42 -0300

    style

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import { React } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import './App.css';
+
+import { Template } from './components/MainComponents';
+import Header       from './components/partials/Header';
+import Footer       from './components/partials/Footer';
+
 import Routes from './Routes';
 
 const Page = (props) => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Template>
+        <Header>
+
+        </Header>
+      
+        <Routes />
+
+        <Footer />
+      </Template>
     </BrowserRouter>
   );
 };
---
+++ src/components/partials/Header/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderArea = styled.div`
+background-color: #FFF;
+height: 60px;
+border-bottom: 1px solid #CCC;  
+
+.container {
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+}
+
+.logo {
+    flex:1;
+    display: flex;
+    align-items: center;
+}
+`;
