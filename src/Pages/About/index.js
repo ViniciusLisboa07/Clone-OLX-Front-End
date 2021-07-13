c8994029b3861340474d3c6c4c4a84b713fa967a
unknown <viniciuslisboa1001@gmail.com>
2021-07-13 16:31:02 -0300

    redux and routes

---
+++ src/Pages/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>
+            Pagina Sobre!!!        
+            </h1>
+            <Link to="/">Home</Link>
+        </div>
+    
+    );
+}
+
+export default Page;
